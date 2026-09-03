f60b441122961c6dd70ea51c07394da34c4b2d18
XtianFreolo <F60@umbc.edu>
2026-09-03 16:47:16 -0400

    added quotecontrollers and quote routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,19 +1,20 @@
 import express from "express";
 import cors from "cors";
 
-const app = express();
+import quoteRoutes from "./routes/quoteRoutes";
 
+const app = express();
 
 app.use(cors());
 
-
 app.use(express.json());
-
 
 app.get("/api/health", (_req, res) => {
     res.status(200).json({
         message: "Cleaning website API is running",
     });
 });
+
+app.use("/api/quotes", quoteRoutes);
 
 export default app;
