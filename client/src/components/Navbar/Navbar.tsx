098e16a153ebf857e0a52be65005bc490fd8ac13
XtianFreolo <F60@umbc.edu>
2026-09-03 16:09:18 -0400

    removed gallery

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -4,23 +4,26 @@ function Navbar() {
     return (
         <header className={styles.header}>
             <nav className={styles.navbar}>
-
+                {/* 
+          Temporary business name.
+          Later we can replace this text with your actual logo image.
+        */}
                 <a href="/" className={styles.logo}>
-                    Cleaning With Wendy
+                    WENDY CLEANING KOMPANY
                 </a>
 
+                {/* Main navigation */}
                 <div className={styles.navLinks}>
                     <a href="#home">Home</a>
                     <a href="#services">Services</a>
                     <a href="#about">About</a>
-                    <a href="#gallery">Gallery</a>
                     <a href="#contact">Contact</a>
                 </div>
 
+                {/* Primary call-to-action */}
                 <a href="#quote" className={styles.quoteButton}>
                     Get a Free Quote
                 </a>
-
             </nav>
         </header>
     );
